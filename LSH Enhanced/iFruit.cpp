6e70134a77257fb9b4bdb434b1cd5b957f16ea61
Liam <160941195+billsyliamgta@users.noreply.github.com>
2025-05-08 12:11:23 +0100

    Update iFruit.cpp

--- LSH Enhanced/iFruit.cpp
+++ LSH Enhanced/iFruit.cpp
@@ -2,6 +2,6 @@
 
 void UI::Scaleforms::LoadScaleform()
 {
-   ScaleformHandle = GRAPHICS::REQUEST_SCALEFORM_MOVIE((char*)gfxMovie.ctr());
+   ScaleformMovieHandle = GRAPHICS::REQUEST_SCALEFORM_MOVIE((char*)gfxMovie.ctr());
    while (!GRAPHICS::HAS_SCALEFORM_MOVIE_LOADED(ScaleformHandle)) WAIT(0);
 }
