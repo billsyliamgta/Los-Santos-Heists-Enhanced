844a79cc93542cd23bc22a3cbb9a85c8317b1feb
Liam <160941195+billsyliamgta@users.noreply.github.com>
2025-05-08 12:19:01 +0100

    Update iFruit.cpp

--- LSH Enhanced/iFruit.cpp
+++ LSH Enhanced/iFruit.cpp
@@ -2,6 +2,16 @@
 
 void UI::Scaleforms::LoadScaleform()
 {
+   int start = GAMEPLAY::GET_GAME_TIMER();
+   int time = 4000;
    ScaleformMovieHandle = GRAPHICS::REQUEST_SCALEFORM_MOVIE((char*)gfxMovie.ctr());
-   while (!GRAPHICS::HAS_SCALEFORM_MOVIE_LOADED(ScaleformHandle)) WAIT(0);
+   while (!GRAPHICS::HAS_SCALEFORM_MOVIE_LOADED(ScaleformMovieHandle))
+   {
+       if (GAMEPLAY::GET_GAME_TIMER() > start + time)
+       {
+           std::string errorS = "ERROR: Timed out while loading Scaleform Movie: " + gfxMovie;
+           throw new std::runtime_error(errorS);
+       }
+       WAIT(0);
+   }
 }
